6e8a2e1dfbb131c9fda9b10c86e303c8a8bb8ec1
Edward <edward.wall@mercateo.com>
2019-07-24 17:07:01 +0200

    fixed equation parser to handle different precendence operators

--- src/EquationParser.hs
+++ src/EquationParser.hs
@@ -5,110 +5,77 @@ module EquationParser where
 
 import AST (ELSE, EQUATION(..), WHENTHEN(..))
 import Control.Applicative ((<|>), liftA2)
-import Data.Attoparsec.ByteString as BP
+import Data.Attoparsec.ByteString as BP (Parser, many', string)
 import Data.ByteString (ByteString)
+import Data.Functor (($>))
+import Data.Maybe (fromJust)
+import Data.Stack
 import Parser
 
-data NParser = NParser
-  { name :: ByteString
-  , psr :: forall a. [NParser] -> Parser a -> Parser (EQUATION, a)
+parseEquation :: Parser a -> Parser (EQUATION, a)
+parseEquation nxt = run
+  where
+    run = do
+      eqs <- BP.many' parseBoolSymbol
+      rest <- liftA2 (,) (VAL . fst) snd <$> anyUntilThat (whitespace *> nxt <* whitespace)
+      pure (reduce (fst rest) eqs, snd rest)
+
+parseBoolSymbol :: Parser (EQUATION, PrecendenceParser)
+parseBoolSymbol = liftA2 (,) (VAL . fst) snd <$> anyUntilThat (whitespace *> psrs <* whitespace)
+  where
+    psrs =
+      (string "=" $> PParser 0 EQU) <|> (string "+" $> PParser 1 PLUS) <|>
+      (string "-" $> PParser 1 MINUS) <|>
+      (string "*" $> PParser 2 TIMES) <|>
+      (string "/" $> PParser 2 DIV)
+
+parseFunction :: Parser a -> Parser (EQUATION, a)
+parseFunction nxt = do
+  name <- anyUntilThat $ string "(" <* whitespace
+  args <- parseArg
+  rest <- nxt
+  return (FUNC (fst name) args, rest)
+  where
+    parseArg :: Parser [EQUATION]
+    parseArg =
+      do arg <- fst <$> parseEquation (string ",")
+         fmap ((:) arg) parseArg
+     <|> pure . fst <$> parseEquation (string ")")
+
+data PrecendenceParser = PParser
+  { prec :: Int
+  , psr :: EQUATION -> EQUATION -> EQUATION
   }
 
-parsers :: [NParser]
-parsers =
-  [ NParser "=" parseEqu
-  , NParser "-" parseMinus
-  , NParser "+" parsePlus
-  , NParser "/" parseDiv
-  , NParser "*" parseTimes
-  ]
-
-parseEquation :: Parser a -> Parser (EQUATION, a)
-parseEquation nxt =
-  parseEqu parsers nxt <|> parseMinus parsers nxt <|> parsePlus parsers nxt <|> parseDiv parsers nxt <|>
-  parseTimes parsers nxt <|>
-  parseVal nxt
-
-parseBoolEqu ::
-     ByteString
-  -> (EQUATION -> EQUATION -> EQUATION)
-  -> [NParser]
-  -> Parser a
-  -> Parser (EQUATION, a)
-parseBoolEqu sym dat psrs nxt = do
-  first <- fst <$> parseExcept psrs sym (whitespace *> string sym <* whitespace)
-  rest <- parseEquation nxt
-  return (dat first (fst rest), snd rest)
-
-parseExcept :: forall a. [NParser] -> ByteString -> Parser a -> Parser (EQUATION, a)
-parseExcept psrs sym nxt = pNew
+reduce :: EQUATION -> [(EQUATION, PrecendenceParser)] -> EQUATION
+reduce end = run stackNew
   where
-    filtered :: [NParser]
-    filtered = filter ((/= sym) . name) psrs
-    pMap :: [Parser (EQUATION, a)]
-    pMap = fmap (\p -> psr p filtered nxt) filtered
-    pNew :: Parser (EQUATION, a)
-    pNew = foldr (<|>) (parseVal nxt) pMap
-
-parseVal :: Parser a -> Parser (EQUATION, a)
-parseVal nxt = liftA2 (,) (VAL . fst) snd <$> anyUntilThat (whitespace *> nxt <* whitespace)
-
-parseEqu :: [NParser] -> Parser a -> Parser (EQUATION, a)
-parseEqu = parseBoolEqu "=" EQU
-
-parsePlus :: [NParser] -> Parser a -> Parser (EQUATION, a)
-parsePlus = parseBoolEqu "+" PLUS
-
-parseMinus :: [NParser] -> Parser a -> Parser (EQUATION, a)
-parseMinus = parseBoolEqu "-" MINUS
-
-parseTimes :: [NParser] -> Parser a -> Parser (EQUATION, a)
-parseTimes = parseBoolEqu "*" TIMES
-
-parseDiv :: [NParser] -> Parser a -> Parser (EQUATION, a)
-parseDiv = parseBoolEqu "/" DIV
--- parseEquation :: Parser a -> Parser (EQUATION, a)
--- parseEquation nxt = parseEqu nxt <|> parsePlus nxt <|> parseVal nxt
---   -- foldr (<|>) (parseVal nxt) . fmap ($ (whitespace *> nxt <* whitespace)) $ parsers
--- 
--- parseSymbolEqu ::
---      ByteString -> (EQUATION -> EQUATION -> EQUATION) -> Parser a -> Parser (EQUATION, a)
--- parseSymbolEqu sym dat nxt = do
---   first <- fst <$> parseVal (whitespace *> string sym <* whitespace)
---   rest <- parseEquation nxt
---   return (dat first (fst rest), snd rest)
--- 
--- parseVal :: Parser a -> Parser (EQUATION, a)
--- parseVal nxt = liftA2 (,) (VAL . fst) snd <$> anyUntilThat (whitespace *> nxt <* whitespace)
--- 
--- parseEqu :: Parser a -> Parser (EQUATION, a)
--- parseEqu nxt -- parseSymbolEqu "=" EQU
---  = do
---   first <-
---     fst <$>
---     (parsePlus (whitespace *> string "=" <* whitespace) <|>
---      parseVal (whitespace *> string "=" <* whitespace))
---   rest <- parseEquation nxt
---   return (EQU first (fst rest), snd rest)
--- 
--- parsePlus :: Parser a -> Parser (EQUATION, a)
--- parsePlus nxt -- parseSymbolEqu "+" PLUS
---  = do
---   first <-
---     fst <$>
---     (parseEqu (whitespace *> string "+" <* whitespace) <|>
---      parseVal (whitespace *> string "+" <* whitespace))
---   rest <- parseEquation nxt
---   return (PLUS first (fst rest), snd rest)
--- parseFunction :: Parser a -> Parser (EQUATION, a)
--- parseFunction nxt = do
---   name <- anyUntilThat $ string "(" <* whitespace
---   args <- parseArg
---   rest <- nxt
---   return (FUNC (fst name) args, rest)
---   where
---     parseArg :: Parser [EQUATION]
---     parseArg =
---       do arg <- fst <$> parseEquation (string ",")
---          fmap ((:) arg) parseArg
---      <|> pure . fst <$> parseEquation (string ")")
+    run :: Stack (EQUATION, PrecendenceParser) -> [(EQUATION, PrecendenceParser)] -> EQUATION
+    run _ [] = end
+    run stk [x@(xe, xpp)] =
+      case stackIsEmpty stk of
+        True -> psr xpp xe end
+        False ->
+          if prec spp >= prec xpp
+            then run stk' [comb s x]
+            else unravel stk (psr xpp xe end)
+          where top'@(stk', s@(se, spp)) = top stk
+    run stk (x@(xe, xpp):y@(ye, ypp):zs) =
+      case stackIsEmpty stk of
+        True ->
+          if prec xpp >= prec ypp
+            then run stk (comb x y : zs)
+            else run (stackPush stk x) (y : zs)
+        False ->
+          if prec xpp >= prec ypp
+            then run stk' (s : comb x y : zs)
+            else run (stackPush stk x) (y : zs)
+          where top'@(stk', s) = top stk
+    unravel :: Stack (EQUATION, PrecendenceParser) -> EQUATION -> EQUATION
+    unravel stk el =
+      case stackIsEmpty stk of
+        True -> el
+        False -> unravel (fst top') (psr (snd . snd $ top') (fst . snd $ top') el)
+          where top' = top stk
+    comb (xe, xpp) (ye, ypp) = (psr xpp xe ye, ypp)
+    top = fromJust . stackPop
--- test/EquationParserSpec.hs
+++ test/EquationParserSpec.hs
@@ -11,30 +11,31 @@ import Data.ByteString (ByteString)
 import qualified EquationParser as P
 import Test.HUnit
 
-tests = TestList [testParseEquation]
+tests = TestList [testParseBoolSymbol, testParseFunction, testParseEquation]
 
--- testParseEqu =
---   "parseEqu" ~:
---   [ "a = 1" ~: Right (A.EQU (A.VAL "a") (A.VAL "1"), ()) ~=?
---     BP.parseOnly (P.parseEqu BP.endOfInput) "a = 1"
---   , "a = b + 2" ~: Right (A.EQU (A.VAL "a") (A.PLUS (A.VAL "b") (A.VAL "2")), ()) ~=?
---     BP.parseOnly (P.parseEqu BP.endOfInput) "a = b + 2"
---   ]
--- testParseFunction =
---   "parseFunction" ~:
---   [ "MAX(id)" ~: Right (A.FUNC "MAX" [A.VAL "id"], ()) ~=?
---     BP.parseOnly (P.parseFunction BP.endOfInput) "MAX(id)"
---   , "IF(id, 0, 1)" ~: Right (A.FUNC "IF" [A.VAL "id", A.VAL "0", A.VAL "1"], ()) ~=?
---     BP.parseOnly (P.parseFunction BP.endOfInput) "IF(id, 0, 1)"
---   , "ABS(-20)" ~: Right (A.FUNC "ABS" [A.VAL "-20"], ()) ~=?
---     BP.parseOnly (P.parseFunction BP.endOfInput) "ABS(-20)"
---   , "CONCAT('Me', 'You', 'Someone')" ~:
---     Right (A.FUNC "CONCAT" [A.VAL "'Me'", A.VAL "'You'", A.VAL "'Someone'"], ()) ~=?
---     BP.parseOnly (P.parseFunction BP.endOfInput) "CONCAT('Me', 'You', 'Someone')"
---   ]
+testParseBoolSymbol =
+  "parseBoolSymbol" ~:
+  [ "a =" ~: Right (A.VAL "a") ~=? fst <$> BP.parseOnly P.parseBoolSymbol "a ="
+  , "a +" ~: Right (A.VAL "a") ~=? fst <$> BP.parseOnly P.parseBoolSymbol "a +"
+  ]
+
+testParseFunction =
+  "parseFunction" ~:
+  [ "MAX(id)" ~: Right (A.FUNC "MAX" [A.VAL "id"], ()) ~=?
+    BP.parseOnly (P.parseFunction BP.endOfInput) "MAX(id)"
+  , "IF(id, 0, 1)" ~: Right (A.FUNC "IF" [A.VAL "id", A.VAL "0", A.VAL "1"], ()) ~=?
+    BP.parseOnly (P.parseFunction BP.endOfInput) "IF(id, 0, 1)"
+  -- , "ABS(-20)" ~: Right (A.FUNC "ABS" [A.VAL "-20"], ()) ~=?
+  --   BP.parseOnly (P.parseFunction BP.endOfInput) "ABS(-20)"
+  , "CONCAT('Me', 'You', 'Someone')" ~:
+    Right (A.FUNC "CONCAT" [A.VAL "'Me'", A.VAL "'You'", A.VAL "'Someone'"], ()) ~=?
+    BP.parseOnly (P.parseFunction BP.endOfInput) "CONCAT('Me', 'You', 'Someone')"
+  ]
+
 testParseEquation =
   "parseEquation" ~:
-  [ "a = 1" ~: Right (A.EQU (A.VAL "a") (A.VAL "1"), ()) ~=?
+  [ "a" ~: Right (A.VAL "a", ()) ~=? BP.parseOnly (P.parseEquation BP.endOfInput) "a"
+  , "a = 1" ~: Right (A.EQU (A.VAL "a") (A.VAL "1"), ()) ~=?
     BP.parseOnly (P.parseEquation BP.endOfInput) "a = 1"
   , "a = b + 2" ~: Right (A.EQU (A.VAL "a") (A.PLUS (A.VAL "b") (A.VAL "2")), ()) ~=?
     BP.parseOnly (P.parseEquation BP.endOfInput) "a = b + 2"
@@ -57,4 +58,11 @@ testParseEquation =
   , "1 = a + 2 * 1" ~:
     Right (A.EQU (A.VAL "1") (A.PLUS (A.VAL "a") (A.TIMES (A.VAL "2") (A.VAL "1"))), ()) ~=?
     BP.parseOnly (P.parseEquation BP.endOfInput) "1 = a + 2 * 1"
+  , "1 / 23 = a + 2 * 1 - b" ~:
+    Right
+      ( A.EQU
+          (A.DIV (A.VAL "1") (A.VAL "23"))
+          (A.MINUS (A.PLUS (A.VAL "a") (A.TIMES (A.VAL "2") (A.VAL "1"))) (A.VAL "b"))
+      , ()) ~=?
+    BP.parseOnly (P.parseEquation BP.endOfInput) "1 / 23 = a + 2 * 1 - b"
   ]
